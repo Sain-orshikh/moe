931128b3621def164f7da8c41b2fee9af7621f23
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2026-03-03 10:57:23 +0800

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/src/app/(assets)/favicon.ico" sizes="any" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
